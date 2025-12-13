77c5b60c5a8557b77ca0dee38f4de13925fb3d3e
Ansaf33 <ansaf_b220172cs@nitc.ac.in>
2025-12-13 07:44:32 +0000

    [FYP-01]: Completed Combined Enrichment

--- src/code_repair.py
+++ src/code_repair.py
@@ -21,28 +21,42 @@ with open(CONFIG_PATH, "r") as f:
 
 
 
-def get_initial_prompt(c_code: str, summary: str,language: str) -> str:
+def get_initial_prompt(c_code: str, function_summary: str, caller_and_callee_summary: str, function_sog: str, language: str) -> str:
   """
   Generate the initial prompt for the repair tool given C code of the particular function.
   """
   initial_prompt = config["prompts"]["system_prompt"]
-  prompt = f"{initial_prompt}\n\n```Language:{language}\nSummary:{summary}\n{c_code}\n```"
+  prompt = f"{initial_prompt}\n\n```Language:{language}\nSummary:{function_summary}\n{c_code}\n```"
+  if caller_and_callee_summary:
+    prompt += f"\n\nCaller and Callee Summary:\n{caller_and_callee_summary}"
+  if function_sog:
+    prompt += f"\n\nFunction SOG:\n{function_sog}"
   return prompt
 
 
-def get_optimized_code(original_c_code: str, summary: str, language: str, max_iterations: int, llm_interface: LLMInterface ) -> str:
+def get_repair_prompt(c_code: str, compilation_errors: str, function_summary: str, caller_and_callee_summary: str, function_sog: str, language: str) -> str:
+  """
+  Generate the repair prompt for the repair tool given C code of the particular function and compilation errors.
+  """
+  repair_prompt = config["prompts"]["compilation_error"]
+  prompt = f"{repair_prompt}\n\n```c\nLanguage:{language}\nSummary:{function_summary}\nCode:{c_code}\n```\n\nCompilation Errors:\n{compilation_errors}\n\nPlease provide the corrected C code."
+  if caller_and_callee_summary:
+    prompt += f"\n\nCaller and Callee Summary:\n{caller_and_callee_summary}"
+  if function_sog:
+    prompt += f"\n\nFunction SOG:\n{function_sog}"
+  return prompt
+
+def get_optimized_code(c_code: str, function_summary: str, caller_and_callee_summary: str, function_sog: str, language: str, max_iterations: int, llm_interface: LLMInterface ) -> str:
   """
   Generate optimized C code using LLM for the given original C code file.
   """
-  
-  compilable_code = original_c_code
-  
+    
   # handle everything in a temporary directory
   with tempfile.TemporaryDirectory() as temp_dir:
     # write the original code to a file
     original_c_file = Path(temp_dir) / "original.c"
     with open(original_c_file, "w") as f:
-      f.write(original_c_code)
+      f.write(c_code)
     
     # check if original code compiles
     status, message = c.compile_source(
@@ -54,12 +68,19 @@ def get_optimized_code(original_c_code: str, summary: str, language: str, max_it
   
     if status:
       print("Original Ghidra Code compiles successfully. No optimization needed.")
-      return True, original_c_code
+      return True, c_code
 
     
     # if not, provide an initial LLM optimization
     print("Original Ghidra Code does not compile. Starting optimization...")
-    initial_prompt = get_initial_prompt(original_c_code,summary,language)
+    initial_prompt = get_initial_prompt(
+      c_code=c_code,
+      function_summary=function_summary,
+      caller_and_callee_summary=caller_and_callee_summary,
+      function_sog=function_sog,
+      language=language
+      )
+    
     optimized_code = llm_interface.generate(initial_prompt)
     
     # check if initially prompted code compiles
@@ -82,7 +103,14 @@ def get_optimized_code(original_c_code: str, summary: str, language: str, max_it
       # acquire optimized output through error passing
       e = ErrorNormalizer()
       error_prompt = e.format_for_llm(message)
-      repair_prompt = f"{config['prompts']['compilation_error']}\n\n```c\nLanguage:{language}\nSummary:{summary}\nCode:{optimized_code}\n```\n\nCompilation Errors:\n{error_prompt}\n\nPlease provide the corrected C code."
+      repair_prompt = get_repair_prompt(
+        c_code=optimized_code,
+        compilation_errors=error_prompt,
+        function_summary=function_summary,
+        caller_and_callee_summary=caller_and_callee_summary,
+        function_sog=function_sog,
+        language=language
+      )
       optimized_code = llm_interface.generate(repair_prompt)
       
       # check if it compiles
--- src/collector_of_program.py
+++ src/collector_of_program.py
@@ -26,29 +26,23 @@ with open(CONFIG_PATH, "r") as f:
 c = Compiler()
 g = Ghidra()
 d = Disassembler()
+'''
 llm_interface = create_llm_interface(
   provider=config["llm"]["gemini_provider"],
   model_name=config["llm"]["gemini_model_name"],
   api_key=config["llm"]["gemini_api_key"]
 )
+'''
+llm_interface = create_llm_interface(
+    provider=config["llm"]["ollama_provider"],
+    model_name=config["llm"]["ollama_model_name"],
+    # api_key=config["llm"].get("ollama_api_key"),  # Not needed for Ollama
+    base_url=config["llm"]["ollama_base_url"]
+)
+
 
 output_dir = Path(config["paths"]["output_path"])
 
-def get_sog_dir(executable_name: str) -> Path:
-    return output_dir / "SOG" / executable_name
-  
-def get_sog_json(executable_name: str, function_name: str) -> Path:
-    sog_dir = get_sog_dir(executable_name)
-    return sog_dir / f"{function_name}.json"
-  
-def get_call_graph_path(executable_name: str) -> Path:
-    sog_dir = get_sog_dir(executable_name)
-    return sog_dir / "call_graph.dot"
-  
-def get_topsort(call_graph_path: Path) -> List[str]:
-    call_graph = build_call_graph(call_graph_path)
-    sorted_functions = topological_sort(call_graph)
-    return sorted_functions
 
 def create_cfg_output_dir(executable_name: str) -> Path:
     output_dir = Path(config["paths"]["output_path"]) / "SOG" / executable_name
@@ -64,7 +58,38 @@ def get_asm_list(executable_path: Path) -> List:
         return {}
     asm_dict = d.function_extraction(asm_code)
     return asm_dict
+
+def get_ghidra_dict(executable_path: Path) -> Dict[str, str]:
+    status, code = g.convert_executable_to_ghidra(executable_path,output_dir=None)
+    if not status:
+        print(f"Ghidra conversion failed for {executable_path.name}")
+        return {}
+    ghidra_dict = create_ghidra_dict(code)
+    return ghidra_dict
+
+def separate_header_and_source(code: str) -> Tuple[str, str]:
+    header = ""
+    source = ""
+    lines = code.splitlines(keepends=True)
+    for line in lines:
+        if line.strip().startswith("#include") or line.strip().startswith("typedef") or line.strip().startswith("struct") or line.strip().startswith("enum"):
+            header += line
+        else:
+            source += line
+    return header, source
+
+def get_program_code(program_data: Dict) -> Dict:
+    program_header = ""
+    program_code = ""
+    combined_code = ""
+    for function in program_data['functions']:
+        header, source = separate_header_and_source(function['optimized_code'])
+        program_header += header + "\n"
+        program_code += source + "\n"
+    combined_code = program_header + "\n" + program_code
     
+    return combined_code
+  
     
   
 def split_enrichment(executable_path: Path):
@@ -76,13 +101,7 @@ def split_enrichment(executable_path: Path):
     
     # -- Acquire ASM and create a dictionary of function_name -> asm
     asm_dict = {f.name: f.instructions for f in get_asm_list(executable_path)}
-    
-    # -- Acquire Ghidra Pseudocode and create a dictionary of function_name -> code
-    status, code = g.convert_executable_to_ghidra(executable_path,output_dir=None)
-    if not status:
-        print(f"Ghidra conversion failed for {executable_name}")
-        return
-    ghidra_dict = create_ghidra_dict(code)
+    ghidra_dict = get_ghidra_dict(executable_path)
     
     
     # -- Acquire SOG and Call Graph    
@@ -91,12 +110,9 @@ def split_enrichment(executable_path: Path):
     callgraph_map = g.extract_call_graph(executable_path, output_dir)
     
     # -- Topologically Sort Call Graph
-    callgraph_path = callgraph_map.get('call_graph')
-    if not callgraph_path:
-        print(f"Call graph extraction failed for {executable_name}")
-        return
-    callgraph = build_call_graph(callgraph_path)
+    callgraph = build_call_graph(callgraph_map.get('call_graph'))
     sorted_functions = topological_sort(callgraph)
+    program_data['callgraph'] = callgraph
     
     # -- For each function in topological order, enrich its SOG using LLM
     functions = []
@@ -117,31 +133,89 @@ def split_enrichment(executable_path: Path):
         callees = callgraph.get(function_name, [])
         f_data['callers'] = callers
         f_data['callees'] = callees
-        functions.append(f_data)
         
         # LLM Guided Enrichment for Summary and Optimized Code
         '''
-        f_data['summary'] = gen_summary(callers, callees, f_data['sog_dot'], f_data['ghidra_code'])
-        status, optimized_code = get_optimized_code(
-            original_c_code=f_data['ghidra_code'],
-            summary=f_data['summary'],
-            language="c",
-            max_iterations=3,
-            llm_interface=llm_interface
-        )
-        f_data['optimized_code'] = optimized_code
-        f_data['optimization_status'] = status
+        Summary for function : Should pass in Ghidra Code, ASM
+        Optimize Code for function : Should pass in Ghidra Code, Summary, SOG
         '''
+        f_data['function_summary'] = gen_code_summary(
+            asm=f_data['asm'],
+            ghidra=f_data['ghidra_code'] 
+        )
+        f_data['optimization_status'], f_data['optimized_code'] = get_optimized_code(
+            c_code=f_data['ghidra_code'],
+            function_summary=f_data['function_summary'],
+            caller_and_callee_summary="",
+            function_sog=f_data['sog_dot'],
+            language="c",
+            llm_interface=llm_interface,
+            max_iterations=3
+        )
+        functions.append(f_data)
         
     program_data['functions'] = functions
         
-    return functions
+    return program_data
 
 
+def combine_enrichment(program_data: Dict) -> str:
+    combined_code = get_program_code(program_data)
+    '''
+    Summary for program : Should pass in combined code
+    Summary for caller_callee_context : Should pass in caller and callee graph
+    Summary for optimized code : Should pass in program code, summary, caller callee context summary
+    '''
+    # get program summary
+    program_summary = gen_code_summary(
+        asm="",
+        ghidra=combined_code
+    )
+    # get caller callee context summary
+    context_summary = gen_context_summary(
+        callgraph=program_data['callgraph']
+    )
+    # get optimized program code
+    status, optimized_program_code = get_optimized_code(
+        c_code=combined_code,
+        function_summary=program_summary,
+        caller_and_callee_summary=context_summary,
+        function_sog="",
+        language="c",
+        llm_interface=llm_interface,
+        max_iterations=3
+    )
+    program_data['program_summary'] = program_summary
+    program_data['context_summary'] = context_summary
+    program_data['optimized_code'] = optimized_program_code
+    program_data['optimization_status'] = status
+    
+    
+    return program_data
 
-def gen_summary(caller_context: List, callee_context: List, function_sog: str, function_ghidra: str) -> str:
+def gen_context_summary(callgraph: Dict[str, List[str]]) -> str:
+    '''
+    context_prompt = config["prompts"]["context_prompt"]
+    prompt = f"{context_prompt}\n\nCall Graph:\n"
+    for function, callees in callgraph.items():
+        prompt += f"{function} calls {', '.join(callees) if callees else 'no functions'}\n"
+    
+    # Call the LLM API to generate a summary
+    response = llm_interface.generate(prompt)
+    '''
+    prompt = ""
+    for function, callees in callgraph.items():
+        prompt += f"{function} calls {', '.join(callees) if callees else 'no functions'}\n"
+    return prompt
+
+
+def gen_code_summary(asm: str, ghidra: str) -> str:
     summary_prompt = config["prompts"]["summary_prompt"]
-    prompt = f"{summary_prompt}\n\nCaller Context: {caller_context}\nCallee Context: {callee_context}\nFunction SOG: {function_sog}\nFunction Ghidra: {function_ghidra}\n"
+    prompt = f"{summary_prompt}"
+    if ghidra:
+        prompt += f"\n\nGhidra Code:\n```c\n{ghidra}\n```"
+    if asm:
+        prompt += f"\n\nAssembly Instructions:\n{asm}"
 
     # Call the LLM API to generate a summary
     response = llm_interface.generate(prompt)
@@ -152,6 +226,8 @@ def main():
     data_dir = Path(config["paths"]["test_path"])
     test_executable = data_dir / "test_3"
     data = split_enrichment(test_executable)
+    data = combine_enrichment(data)
+    
     with open(str(Path(output_dir) / f"enriched_data_{test_executable.stem}.json"), "w") as f:
         json.dump(data, f, indent=4)
 
